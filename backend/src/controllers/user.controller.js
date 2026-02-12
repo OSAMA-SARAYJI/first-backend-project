64d669b434a0308402fc061c3ffb4b421c314178
Osama Sarayji <osarayji10@gmail.com>
2026-02-12 22:44:08 +0300

    finish auth apis

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -39,4 +39,20 @@ catch(error){
     res.status(500).json({message:"Internal server error",error:error.message}); 
 }
 }
-export {registerUser,loginUser}
+
+const logoutUser = async(req,res) => {
+    try{
+        const {email} = req.body;
+        const user = await User.findOne({
+            email:email
+        });
+        if(!user){
+            return res.status(400).json({message:"User not found!"});
+        }
+        user.loggedIn = false;
+        await user.save();
+        res.status(200).json({message:"Logged out successfully"});
+    } catch(error){
+        res.status(500).json({message:"Internal server error",error:error.message});}
+    }
+export {registerUser,loginUser,logoutUser}
